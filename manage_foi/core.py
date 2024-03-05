ebb3edab06fb67e69f91f25e2e4dad99e8b4929a
maxrisher <max.risher@gmail.com>
2024-03-05 12:48:52 +0100

    renamed project

---
+++ manage_foi/core.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from . import helpers
+
+class lpa_conversation:
+    def __init__(self, lpa_name, lpa_email):
+        self.lpa_name = lpa_name
+        self.lpa_email = lpa_email
+
+    def all_associated_emails():
+        return 
+    
+    def has_replied()
+        return
+
+
+def get_hmm():
+    """Get a thought."""
+    return 'hmmm...'
+
+
+def hmm():
+    """Contemplation..."""
+    if helpers.get_answer():
+        print(get_hmm())
